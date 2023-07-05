a788f9aee8b1cdd2374e27808c9fd15a4c4b7df5
vagrant <hassan.a.issak@gmail.com>
2023-07-05 20:43:45 +0000

    Refactors code in client to add Move:Up

--- client.js
+++ client.js
@@ -8,7 +8,8 @@ const connect = function () {
   conn.setEncoding("utf8");
   conn.on("connect", (data) => {
     console.log("Successfully connected to game server");
-          conn.write("Name: HAI");
+          conn.write("Name: SNK");
+          // conn.write("Move: up");
       });
     conn.on("data", (data) => {
         console.log(data)
