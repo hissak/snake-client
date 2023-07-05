62d139fae4e98b6f057ced150a146ae909da4985
vagrant <hassan.a.issak@gmail.com>
2023-07-05 20:25:40 +0000

    Created play and client

---
+++ client.js
@@ -0,0 +1,18 @@
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // IP address here,
+    port: '50541',// PORT number here,
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", (data) => {
+    console.log("Successfully connected to game server");
+          conn.write("Name: HI");
+      });
+    conn.on("data", (data) => {
+        console.log(data)
+          })  
+  return conn;
+};
+
+module.exports = {connect}
