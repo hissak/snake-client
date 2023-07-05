3e2dae8cabdbcce666e3ec0d515ba98fea8d979f
vagrant <hassan.a.issak@gmail.com>
2023-07-05 20:55:02 +0000

    Adds setupInput with handleUserInput

--- play.js
+++ play.js
@@ -3,3 +3,21 @@ const {connect} = require('./client')
 console.log("Connecting ...");
 connect();
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function (input) {
+    if (input === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
+setupInput()
+
